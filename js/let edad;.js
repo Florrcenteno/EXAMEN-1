4cbff1f653f45b7e9154c5994334092f2ec83ca7
Florrcenteno <114961309+Florrcenteno@users.noreply.github.com>
2022-10-03 20:26:58 -0300

    Add files via upload

---
+++ js/let edad;.js
@@ -0,0 +1,8 @@
+let edad;
+edad= Number((prompt("Introduce tu edad"));
+if (edad >18) {
+    alert("Puedes entrar, eres mayor de edad.");
+    else{
+        alert("No puedes entrar, eres menor de edad.") 
+    }
+}
